f4e2156725f68ffc52181100c31dcd8b4ebe7a31
kevcast95 <kevcast06@gmail.com>
2022-03-21 13:12:25 -0500

    autoplay

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 function addUp () {
-    const msj = document.getElementById("msj");
-    console.log(msj);
-    setTimeout(() => {
-        msj.classList.remove("none");
-        msj.classList.add("show")
-    }, 3000);
+    document.getElementById("video").play();
 }
 
 addUp()
