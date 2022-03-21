e55fcb10cedd65c3546ce501fe5b52adb17d6940
kevcast95 <kevcast06@gmail.com>
2022-03-21 17:18:44 -0500

    last update

--- index.js
+++ index.js
@@ -1,5 +1,403 @@
-function addUp () {
+const guests = [
+    {
+        "name ": "Miryam Jaller",
+        "quota": "1",
+        "code": "1234"
+    },
+    {
+        "name ": "Osvaldo Olave",
+        "quota": "1",
+        "code": "1235"
+    },
+    {
+        "name ": "Flia. Jaller Chacón",
+        "quota": "2",
+        "code": "1236"
+    },
+    {
+        "name ": "Flia. Jaller Fuentes",
+        "quota": "1",
+        "code": "1237"
+    },
+    {
+        "name ": "Flia. Chacon Ruíz",
+        "quota": "2",
+        "code": "1238"
+    },
+    {
+        "name ": "Tia Maria Magdalena Luna",
+        "quota": "1",
+        "code": "1239"
+    },
+    {
+        "name ": "Flia. Machado",
+        "quota": "2",
+        "code": "1240"
+    },
+    {
+        "name ": "Flia. Barraza Machado",
+        "quota": "2",
+        "code": "1241"
+    },
+    {
+        "name ": "Flia. Goenaga Jaller",
+        "quota": "2",
+        "code": "1242"
+    },
+    {
+        "name ": "Tio Jesus Jaller",
+        "quota": "1",
+        "code": "1243"
+    },
+    {
+        "name ": "Flia. Herazo Quevez",
+        "quota": "2",
+        "code": "1244"
+    },
+    {
+        "name ": "Sra. Delfina Corena",
+        "quota": "1",
+        "code": "1245"
+    },
+    {
+        "name ": "Flia. Barrios Laborda",
+        "quota": "4",
+        "code": "1246"
+    },
+    {
+        "name ": "Sr. José Consuegra Bolívar Y Sra",
+        "quota": "2",
+        "code": "1247"
+    },
+    {
+        "name ": "Flia. Consuegra Bayuelo",
+        "quota": "2",
+        "code": "1248"
+    },
+    {
+        "name ": "Flia. Arroyo Andrade",
+        "quota": "2",
+        "code": "1249"
+    },
+    {
+        "name ": "Francisco Arroyo",
+        "quota": "1",
+        "code": "1250"
+    },
+    {
+        "name ": "Flia. Arroyo Barbosa",
+        "quota": "4",
+        "code": "1251"
+    },
+    {
+        "name ": "Flia. Valentino Andrade",
+        "quota": "2",
+        "code": "1252"
+    },
+    {
+        "name ": "Josefa Reales",
+        "quota": "1",
+        "code": "1253"
+    },
+    {
+        "name ": "Ivan Agudelo",
+        "quota": "1",
+        "code": "1254"
+    },
+    {
+        "name ": "Flia. Andrade Lugo",
+        "quota": "4",
+        "code": "1255"
+    },
+    {
+        "name ": "Flia. Andrade Cantillo",
+        "quota": "4",
+        "code": "1256"
+    },
+    {
+        "name ": "Héctor Andradre Arroyo",
+        "quota": "1",
+        "code": "1257"
+    },
+    {
+        "name ": "Daniel Sandoval Andrade",
+        "quota": "1",
+        "code": "1258"
+    },
+    {
+        "name ": "Flia. Alvarez Reales",
+        "quota": "2",
+        "code": "1259"
+    },
+    {
+        "name ": "Flia. Cardenas Alvarez",
+        "quota": "2",
+        "code": "1260"
+    },
+    {
+        "name ": "Flia. Ramirez Alvarez",
+        "quota": "2",
+        "code": "1261"
+    },
+    {
+        "name ": "Flia. Orobajo Montes",
+        "quota": "2",
+        "code": "1262"
+    },
+    {
+        "name ": "Flia. Arroyo Ricardo",
+        "quota": "2",
+        "code": "1263"
+    },
+    {
+        "name ": "Damaris Arroyo Guzman",
+        "quota": "1",
+        "code": "1264"
+    },
+    {
+        "name ": "Flia. Zapata Arroyo",
+        "quota": "2",
+        "code": "1265"
+    },
+    {
+        "name ": "Flia. Arroyo Olivera",
+        "quota": "2",
+        "code": "1266"
+    },
+    {
+        "name ": "Tía Dorgy Arroyo Guzman",
+        "quota": "1",
+        "code": "1267"
+    },
+    {
+        "name ": "Flia. Sanz Reales",
+        "quota": "2",
+        "code": "1268"
+    },
+    {
+        "name ": "Flia. Morron Reales",
+        "quota": "2",
+        "code": "1269"
+    },
+    {
+        "name ": "Flia.Reales Turizo",
+        "quota": "2",
+        "code": "1270"
+    },
+    {
+        "name ": "Flia. Reales Guerra",
+        "quota": "2",
+        "code": "1271"
+    },
+    {
+        "name ": "Flia. Banquez",
+        "quota": "2",
+        "code": "1272"
+    },
+    {
+        "name ": "Ana Mercado",
+        "quota": "1",
+        "code": "1273"
+    },
+    {
+        "name ": "Maria Mercado",
+        "quota": "1",
+        "code": "1274"
+    },
+    {
+        "name ": "Flia. Álvarez Martínez",
+        "quota": "2",
+        "code": "1275"
+    },
+    {
+        "name ": "Flia. Alvarez Castro",
+        "quota": "2",
+        "code": "1276"
+    },
+    {
+    "name ": "Flia. Lugo Agudelo",
+    "quota": "4",
+    "code": "1277"
+    },
+    {
+        "name ": "Aleandra Montaño",
+        "quota": "1",
+        "code": "1278"
+    },
+    {
+        "name ": "Maria Andrea Spadafora",
+        "quota": "1",
+        "code": "1279"
+    },
+    {
+        "name ": "Nareth Dajil Quintero",
+        "quota": "1",
+        "code": "1280"
+    },
+    {
+        "name ": "Amy Ahumada",
+        "quota": "1",
+        "code": "1281"
+    },
+    {
+        "name ": "Flia. De La Ossa Angulo",
+        "quota": "2",
+        "code": "1282"
+    },
+    {
+        "name ": "Wilson Bohorquez",
+        "quota": "1",
+        "code": "1283"
+    },
+    {
+        "name ": "Daniel Gomez Rivero",
+        "quota": "1",
+        "code": "1284"
+    },
+    {
+        "name ": "Kevin Castiblanco Y Daniela Gaviria",
+        "quota": "2",
+        "code": "1285"
+    },
+    {
+        "name ": "Nicolas Rodriguez Y Julieth Cepeda",
+        "quota": "2",
+        "code": "1286"
+    },
+    {
+        "name ": "Carolina Palomino Fonseca",
+        "quota": "1",
+        "code": "1287"
+    },
+    {
+        "name ": "Flia. Lara Solano",
+        "quota": "2",
+        "code": "1288"
+    },
+    {
+        "name ": "José Alejandro Espinosa",
+        "quota": "1",
+        "code": "1289"
+    },
+    {
+        "name ": "Cesar Bossio",
+        "quota": "1",
+        "code": "1290"
+    },
+    {
+        "name ": "Fernando Rodriguez Y Andrea Mendoza",
+        "quota": "2",
+        "code": "1291"
+    },
+    {
+        "name ": "Pablo Abello Y Hellen Contreras",
+        "quota": "2",
+        "code": "1292"
+    },
+    {
+        "name ": "Flia. Rangel Gomez",
+        "quota": "2",
+        "code": "1293"
+    },
+    {
+        "name ": "Flia. Pincetti Casadiego",
+        "quota": "2",
+        "code": "1294"
+    },
+    {
+        "name ": "Flia. Donoso Elles",
+        "quota": "2",
+        "code": "1295"
+    },
+    {
+        "name ": "Flia. Muñoz Vergara",
+        "quota": "2",
+        "code": "1296"
+    },
+    {
+        "name ": "Flia. Romero Gutierrez",
+        "quota": "2",
+        "code": "1297"
+    },
+    {
+        "name ": "Flia. Martinez Madrigal",
+        "quota": "2",
+        "code": "1298"
+    },
+    {
+        "name ": "Flia. Eljagh Tapia",
+        "quota": "2",
+        "code": "1299"
+    },
+    {
+        "name ": "Elizabeth Garcia",
+        "quota": "1",
+        "code": "1300"
+    },
+    {
+        "name ": "Flia. Cordero Rodríguez",
+        "quota": "2",
+        "code": "1301"
+    },
+    {
+        "name ": "Cindy Demoya",
+        "quota": "1",
+        "code": "1302"
+    },
+    {
+        "name ": "Felipe Ruiz",
+        "quota": "1",
+        "code": "1303"
+    },
+    {
+        "name ": "Sr. Jose Forero",
+        "quota": "1",
+        "code": "1304"
+    },
+    {
+        "name ": "Jose Forero Y Carolina Sierra",
+        "quota": "2",
+        "code": "1305"
+    },
+    {
+        "name ": "Loui Ahumada",
+        "quota": "1",
+        "code": "1306"
+    },
+    {
+        "name ": "Valeria Cotes",
+        "quota": "1",
+        "code": "1307"
+    },
+]
+console.table(guests)
+function addUp() {
     document.getElementById("video").play();
 }
 
+function setTop() {
+    const validate = document.getElementById("input-contianer");
+    validate.scrollIntoView({ behavior: 'smooth' })
+}
+
+function validateInvitate() {
+    const body = document.querySelector("body")
+    body.style.overflow = "auto"
+    const validate = document.getElementById("main");
+    const code  = document.getElementById("code").value;
+    const guest = guests.filter(gst => gst.code === code);
+    const guestName = document.getElementById("guest-name");
+    const quota = document.getElementById("quota")
+    if(guest.length > 0){
+        validate.scrollIntoView({ behavior: 'smooth' })
+        guestName.innerText = guest[0]["name "];
+        quota.innerHTML = `(${guest[0].quota})`;
+    }else {
+       alert("Por favor verificar código")
+       return
+   }
+    console.log(code);
+}
+
 addUp()
+setTop()
